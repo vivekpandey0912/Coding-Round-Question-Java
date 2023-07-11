a41e80b34d736d945f43f81177df390067a284ca
Vivek pandey <vp1446392@gmail.com>
2023-07-11 17:25:18 +0530

    coding round question 02 Reverse of String

--- src/Main.java
+++ src/Main.java
@@ -30,3 +30,52 @@ class Solution1 {
     }
 }
 
+//  Write a program to reverse of String
+
+
+class PrintString
+{
+    public static void printReverseStringWithStringBuffer(String value)
+    {
+
+        StringBuffer stringBuffer = new StringBuffer(value);
+        System.out.println(stringBuffer.reverse());
+
+
+
+    }
+    public static void printReverseStringWithLoop(String value)
+    {
+
+        for (int i = value.length()-1; i>=0; i--) {
+
+            System.out.println(value.charAt(i));
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+        String name = "Vivek Pandey";
+        String name1 = "Alok Pandey";
+
+        PrintString.printReverseStringWithStringBuffer(name);
+        PrintString.printReverseStringWithLoop(name1);
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
