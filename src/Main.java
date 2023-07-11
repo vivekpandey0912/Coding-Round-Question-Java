0e188518c011480b65603b5a7c67ddb8bd8dfc90
Vivek pandey <vp1446392@gmail.com>
2023-07-11 17:19:23 +0530

    One question added for print element of integer array and string array withs same method

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+package src;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+// write a program to print string and array with same method
+class Solution1 {
+
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+
+        printArray(intArray);
+        printArray(stringArray);
+    }
+}
+
